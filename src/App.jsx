92e34ad06d80556adb7f2ca776112255dfdd07b3
hanleymark <home@markhanley.co.uk>
2023-06-13 10:46:32 +0100

    Initial setup of react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 const App = () => {
   return (
     <>
-      <p>Emoji Sweeper</p>
+      <p>Emoji Sweeper<span>&#x1F4a3;</span></p>
     </>
   );
 };
 
 export default App;
+
+
+/*
+<p>Bomb &#x1F4a3;</p>
+<p>Smiley &#128512;</p>
+<p>Flag &#x1F6a9;</p>
+<p>Skull &#x1F480;</p>
+<p>Dead smiley &#x1f635</p>
+<p>Explosion &#x1F4a5;</p>
+*/
