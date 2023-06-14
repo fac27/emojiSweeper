f3b170f049f5c04e4633897ef07efd119cddcd02
Zakaria Yaris <info@zakariya.me>
2023-06-14 11:59:44 +0100

    Refactored BoardHeader component to use game state from Game component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
-import Board from "./Board";
+import { useState } from 'react';
+import BoardHeader from './BoardHeader';
+import Board from './Board';
 
 export default function Game() {
-    const [gameState, setGameState] = useState(
-        {
-            width: 8,
-            height: 8,
-            numberOfBombs: 10,
-            isGameOver: false,
-            isGameWon: false,
-            isGameStarted: false,
-        });
+  const [gameState, setGameState] = useState({
+    width: 8,
+    height: 8,
+    numberOfBombs: 10,
+    isGameOver: false,
+    isGameWon: false,
+    isGameStarted: false,
+  });
 
-    return (
+  return (
     <>
-        <Board gameState={gameState} setGameState={setGameState} />
+      <BoardHeader gameState={gameState} setGameState={setGameState} />
+      <Board gameState={gameState} setGameState={setGameState} />
     </>
-    );
+  );
 }
